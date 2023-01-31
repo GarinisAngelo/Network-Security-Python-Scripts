de181fe9ffaf2efd0b0d9e969cd480d0ec918d87
GarinisAngelo <124051601+GarinisAngelo@users.noreply.github.com>
2023-01-31 14:02:57 +0200

    Add files via upload

---
+++ create_threads.py
@@ -0,0 +1,13 @@
+import threading
+
+
+def create_threads(iplist, function):
+    threads = []
+
+    for ip in iplist:
+        th = threading.Thread(target=function, args=(ip,))
+        th.start()
+        threads.append(th)
+
+    for th in threads:
+        th.join()  # Waiting for all the threads to finish
